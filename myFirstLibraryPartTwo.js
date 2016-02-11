66a43529d8b64833fa90e11ce6a0ddba7c1a5dfc
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-02-10 19:28:25 -0800

    refactor loopThrough

--- myFirstLibraryPartTwo.js
+++ myFirstLibraryPartTwo.js
@@ -1,49 +1,110 @@
-/*
+// =============================LECTURE=================================
 
-Objective: Build out the rest of these functions without using a for loop.
+// ===============LOOPTHROUGH CHECKING ARRAY ONLY========================
+var loopThrough = function(array, callback){
+	for(var i = 0; i < array.length; i++){
+		callback(array[i]);
+	}
+};
+// ============FILTERBY (NOT USING ANONYMOUS FUNCTION)===================
 
-
-*/
-
-var loopThrough = function(collection, callback){
-	/* loopthrough, refactored:
-
-		loopThrough takes a collection (array, or object), and a callback, invokes the callback function on each element in the collection.
-		
-		EXAMPLE:
-
-		var obj = {course: 'Telegraph Prep+', best: true};
-		
-		var arr = [1, 2, 3, 4, 5];
-	
-		var print = function(val){
-			console.log(val);
-		};
-
-		loopThrough(print, obj)
-		// Telegraph Prep
-		// true
-
-		loopThrough(print, arr)
-		// 1
-		// 2
-		// 3
-		// 4
-		// 5
-	
-	*/
+// filterBy takes an array, a predicate(just a function that returns true or false) and returns all the elements that pass the truth test
+startsWithA = function(val){
+	return val[0] === 'a';
 };
 
+var names = ['albrey', 'diana', 'adam', 'jeff'];
 
+// filterBy(names, startsWithA);
+// returns ['albrey', 'adam']
+
+var filterBy = function(array, predicate){
+	// create an empty array that stores the values that pass the predicate test
+	var result = [];
+
+	// collectResult function will test the predicate for each value in the array and push the value if predicate is true
+	var collectResult = function(val){
+		if(predicate(val)){
+			result.push(val);
+		}
+	};
+	// loop through the array
+	loopThrough(array, collectResult);
+
+	return result;
+};
+
+filterBy(names, startsWithA);
+
+// ===FILTERBY (USING ANONYMOUS FUNCTION, CHECKING OBJECT AND ARRAYS====
+var loopThrough = function(collection, callback){
+	if(Array.isArray(collection)){
+		for(var i = 0; i < collection.length; i++){
+		callback(array[i], i);
+	}
+	else {
+		for(var key in collection){
+			callback(collection[key], key);
+		}
+	}
+};
+
+var greetings = ["hey", "whatsup", "hello", "hi"];
+
+indexOf(arr, 5){
+
+}
+
+varPrintValAndIndex = function(val, index){
+	console.log(val, index);
+}
+
+var arr = ["hi", "hey"];
+loopThrough(arr, printValAndIndex);
+// "hi", 0
+// "hey", 1
+
+/*
+======================myFirstLibraryPartTwo=========================
+Objective: Build out the rest of these functions without using a for loop.
+*/
+
+var obj = {course: 'Telegraph Prep+', best: true};
+var arr = [1, 2, 3, 4, 5];
+var print = function(val){
+	console.log(val);
+};
+
+// loopThrough takes a collection (array, or object), and a callback, invokes the callback function on each element in the collection
+var loopThrough = function(collection, callback){
+	if(Array.isArray(collection)){
+		for(var i = 0; i < collection.length; i++){
+			callback(collection[i], i);
+		}
+	}
+	else {
+		for(var key in collection){
+			callback(collection[key], key);
+		}
+	}
+};
+
+// loopThrough(obj, print);
+	// Telegraph Prep
+	// true
+
+// loopThrough(arr, print);
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
 
 /* Test copyBy, filterBy with Objects:
 
 Import (copy and paste) your filterBy and copyBy functions and invoke them with callbacks that make sense to be used on objects.
 
-
 */
-
-
 var containsValue = function(collection, value){
 	/*
 	
